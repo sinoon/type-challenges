f2c5eb3e465922b57a6c6b60f4b14caedac9d4b7
沧浪 <6065488+sinoon@users.noreply.github.com>
2020-09-11 01:51:09 +0000

    feat(question): add #191 - Append argument to Function

---
+++ questions/191-easy-append-argument-to-function/test-cases.ts
@@ -0,0 +1,12 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type SomeF = (a: number, b: string) => number
+type FinalF = AppendArgument<SomeF, boolean> 
+
+interface T {
+    (x: boolean, a: number, b: string) => number
+}
+
+type cases = [
+  Expect<Equal<FinalF, T>>
+]
