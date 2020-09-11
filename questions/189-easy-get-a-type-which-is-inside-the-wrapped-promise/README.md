d5c7234e448bc5ec3fc86ea9df42862b55d487dd
沧浪 <6065488+sinoon@users.noreply.github.com>
2020-09-11 01:14:34 +0000

    feat(question): add #189 - Get a type which is inside the wrapped Promise

---
+++ questions/189-easy-get-a-type-which-is-inside-the-wrapped-promise/README.md
@@ -0,0 +1,3 @@
+If we have a type which is wrapped type like Promise. How we can get a type which is inside the wrapped type? For example if we have Promise<ExampleType> how to get ExampleType?
+
+impower by: [@macsikora](https://twitter.com/macsikora), [atrical](https://dev.to/macsikora/advanced-typescript-exercises-question-1-45k4)
---
+++ questions/189-easy-get-a-type-which-is-inside-the-wrapped-promise/test-cases.ts
@@ -0,0 +1,12 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+
+type ResultX = Transform<X>; // ResultX type equals string
+type ResultY = Transform<Y>; // ResultY type equals { field: number }
+
+type cases = [
+  Expect<Equal<ResultX, string>>
+  Expect<Equal<ResultY, { field: number }>>
+]
